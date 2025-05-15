1eabc911532c1081a703d3b6e02f89cb24286793
Garrett Williams <garrettdw14497@outlook.com>
2025-05-14 20:20:57 -0400

    storage system and internal uart

---
+++ Emu6502/RS232Interface.cs
@@ -0,0 +1,56 @@
+﻿namespace Emu6502
+{
+    public class RS232Interface
+    {
+        public readonly RS232Interface pairedWith;
+        private readonly object lck = new object();
+
+        private bool available = false;
+        private byte data = 0;
+
+        public RS232Interface()
+        {
+            pairedWith = new RS232Interface(this);
+        }
+
+        private RS232Interface(RS232Interface pairedWith)
+        {
+            this.pairedWith = pairedWith;
+        }
+
+        public static (RS232Interface, RS232Interface) CreatePair()
+        {
+            RS232Interface portA = new RS232Interface();
+
+            return (portA, portA.pairedWith);
+        }
+
+        public bool Available()
+        {
+            return available;
+        }
+
+        public bool ClearToSend()
+        {
+            return pairedWith.available;
+        }
+
+        public byte Read()
+        {
+            lock (lck)
+            {
+                available = false;
+                return data;
+            }
+        }
+
+        public void Write(byte value)
+        {
+            lock (pairedWith.lck)
+            {
+                pairedWith.data = value;
+                pairedWith.available = true;
+            }
+        }
+    }
+}
---
+++ Emu6502/SerialDrive.cs
@@ -0,0 +1,89 @@
+﻿using System.Text;
+
+namespace Emu6502
+{
+    /// <summary>
+    /// Implements a drive which manages a file system and allows access to it over a serial interface. See devices such as the Commadore 1541 etc.
+    /// </summary>
+    public class SerialDrive
+    {
+        public RS232Interface ExternalPort => port.pairedWith;
+
+        byte START_COMMAND = (byte)'>';
+        byte END_COMMAND = (byte)'<';
+
+        private readonly RS232Interface port = new RS232Interface();
+
+        private State currentState = State.idle;
+        private List<byte> currentCommand = new List<byte>();
+
+        public SerialDrive()
+        {
+
+        }
+
+        public void OnCycle(int hz)
+        {
+            if (!port.Available())
+                return;
+
+            ProgressState(port.Read());
+        }
+
+        private void ProgressState(byte input)
+        {
+            if (currentState == State.idle)
+            {
+                if (input != START_COMMAND)
+                    return;
+
+                currentState = State.receivingCommand;
+            }
+            else if (currentState == State.receivingCommand)
+            {
+                if (input != END_COMMAND)
+                {
+                    ValidifyCommandInput(input);
+                    currentCommand.Add(input);
+                }
+                else
+                    HandleCommand();
+            }
+        }
+
+        private bool ValidifyCommandInput(byte input)
+        {
+            return input > 32 && input < 127;
+        }
+
+        private void HandleCommand()
+        {
+            string command = currentCommand.ToString();
+
+            // Reads from an existing file.
+            if (command == "read")
+            {
+
+            }
+            // Creates a new file or overwrites the existing file.
+            else if (command == "write")
+            {
+                
+            }
+            // Appends to an existing file.
+            else if (command == "append")
+            {
+
+            }
+        }
+
+        private enum State
+        {
+            // Waiting for the next command.
+            idle,
+            // Receiving a command.
+            receivingCommand,
+            
+        }
+    }
+}
