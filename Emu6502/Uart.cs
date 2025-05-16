87cd73ae971d7d7b46480f19e0d03bf6d73216b7
Garrett Williams <garrettdw14497@outlook.com>
2025-05-16 03:22:32 -0400

    fixed uart bug not handling transfers when not accessed by cpu

--- Emu6502/Uart.cs
+++ Emu6502/Uart.cs
@@ -103,12 +103,12 @@ namespace Emu6502
 
         public override void OnCycle(IDeviceInterface bc)
         {
+            HandleTransfers(bc.Hz);
+
             if (!InRange(bc.Address))
                 return;
 
             ushort reg = Relative(bc.Address);
-
-            HandleTransfers(bc.Hz);
 
             if (bc.Rwb)
             {
